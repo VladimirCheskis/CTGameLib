018e2cb9ef5924714865adf5d5a961485f34bbb4
Vladimir <vcheskis>
2016-09-29 17:22:00 +0300

    Remove sprites from main

--- App1/main.cpp
+++ App1/main.cpp
@@ -25,26 +25,6 @@ int main()
 
     /// Создаёт и добавляет в сцену спрайты, создаь для них скрипты
 
-    /// Создаём спрайт демона и добавляем его в сцену. 100, 120 - координаты
-    auto demon = createSpriteDemon(100, 120);
-    gameScene.addObject(demon);
-
-    /// Создаём спрайт волшебника и добавляем его в сцену. 100, 120 - координаты
-    auto wizard = createSpriteWizard(500, 120);
-    gameScene.addObject(wizard);
-
-    /// Когда спрайт кликнут мышью, переместить спрайт в координаты спрайта sprite1
-    auto clickAction = make_shared<OnSpriteMouseClickAction>(demon);
-    clickAction->setIsRepeatable(true);
-    clickAction->addAction(make_shared<SpriteActionAnimate>(demon, test, EAnimationType::Die));
-    clickAction->addAction(make_shared<SentMessageAction>(demon, wizard , "Привет!"));
-    demon->addAction(clickAction);
-
-    auto onMessage = make_shared<OnMessageAction>(wizard, "Привет!");
-    onMessage->addAction(make_shared<SpriteActionMoveByVector>(wizard, 50, 0, 1));
-    onMessage->setIsRepeatable(true);
-    wizard->addAction(onMessage);
-
     /// Игровой цикл: обрабатываем сообщения, проигрываем команды,
     /// пока окно не закроют
 	while (gameScene.tick());
